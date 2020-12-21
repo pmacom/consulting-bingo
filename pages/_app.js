be4b3282aeafd4e2db0177c69f86bd83db2be55b
Aj J May <AJ.May@ey.com@usc02df5jemd6t.lan>
2020-12-20 23:46:19 -0500

    feat: add basic bingo functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'fontsource-roboto';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import theme from '../theme';
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return <>
+    <Head>
+      <title>Consultant Bingo</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+    </Head>
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>;
+};
+
+export default App;
